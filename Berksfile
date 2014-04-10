8d03cf18fe4fcb2e022e9161f0941d39edbce3e6
Jason Boyles <jason@duncancreek.net>
2014-04-10 09:36:45 -0500

    initial commit

---
+++ Berksfile
@@ -0,0 +1,10 @@
+site :opscode
+
+cookbook 'apt'
+cookbook 'build-essential'
+
+cookbook 'redisio',
+  :git => 'https://github.com/brianbianco/redisio.git'
+  :branch => '2.0.0_wip'
+
+metadata
---
+++ recipes/config_slave_sentinel.rb
@@ -0,0 +1,12 @@
+
+include_recipe 'apt::default'
+include_recipe 'build-essential::default'
+
+include_recipe 'redisio::default'
+include_recipe 'redisio::enable'
+include_recipe 'redisio::sentinel'
+include_recipe 'redisio::sentinel_enable'
+
+service 'redis_sentinel_#{node[:redisio][:sentinels][0][:name]}'
+  action [ :enable, :start ]
+end
