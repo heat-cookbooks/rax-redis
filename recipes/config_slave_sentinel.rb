9cee972763a9a316858f7093bae5824ac182bb9b
Jason Boyles <jason@duncancreek.net>
2014-04-11 17:21:01 -0500

    hackishly forcibly start sentinel. I'm sorry.

--- recipes/config_slave_sentinel.rb
+++ recipes/config_slave_sentinel.rb
@@ -10,3 +10,10 @@ include_recipe 'redisio::sentinel_enable'
 service "redis_sentinel_#{node[:redisio][:sentinels][0][:name]}" do
   action [ :enable, :start ]
 end
+
+bash "forcibly start sentinel" do
+  cwd "/tmp"
+  code <<-EOH
+  /etc/init.d/redis_sentinel_#{node[:redisio][:sentinels][0][:name]} start
+  EOH
+end
